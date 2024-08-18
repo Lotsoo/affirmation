597e88e08b48cf9c1137e3222ebbcd477440d612
lotsoo <braindeathrama@gmail.com>
2024-08-18 10:18:22 +0700

    feat: created data affirmation

---
+++ app/src/main/java/com/example/affirmation/data/DataSource.kt
@@ -0,0 +1,19 @@
+package com.example.affirmation.data
+
+import com.example.affirmation.R
+import com.example.affirmation.model.Affirmation
+
+object DataSource {
+    val affirmations = listOf(
+        Affirmation(title = R.string.affirmation1, image = R.drawable.image1),
+        Affirmation(title = R.string.affirmation2, image = R.drawable.image2),
+        Affirmation(title = R.string.affirmation3, image = R.drawable.image3),
+        Affirmation(title = R.string.affirmation4, image = R.drawable.image4),
+        Affirmation(title = R.string.affirmation5, image = R.drawable.image5),
+        Affirmation(title = R.string.affirmation6, image = R.drawable.image6),
+        Affirmation(title = R.string.affirmation7, image = R.drawable.image7),
+        Affirmation(title = R.string.affirmation8, image = R.drawable.image8),
+        Affirmation(title = R.string.affirmation9, image = R.drawable.image9),
+        Affirmation(title = R.string.affirmation10, image = R.drawable.image10),
+    )
+}
